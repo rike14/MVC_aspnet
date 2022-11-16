4eba03081601b92a430965c2a347c995b4741aba
Henrique Kronhardt <henriquemk00@gmail.com>
2022-11-15 21:18:55 -0300

    Inserindo messagens de erro

--- CRUD/Models/Pessoa.cs
+++ CRUD/Models/Pessoa.cs
@@ -7,7 +7,6 @@ namespace CRUD.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [MaxLength(61, ErrorMessage = "O campo {0} não pode ser maior que {1} caracteres")]
         [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} tem que ter entre {2} e {1} caracteres")]
         public string Nome { get; set; }
 
@@ -17,7 +16,6 @@ namespace CRUD.Models
         public long Cpf { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [MaxLength(151, ErrorMessage = "O campo {0} não pode ser maior que {1} caracteres")]
         [StringLength(150, MinimumLength = 15, ErrorMessage = "{0} tem que ter no mínimo {2} caracteres")]
         public string Endereco { get; set; }
         //public List<Telefone> Telefones { get; set; }
