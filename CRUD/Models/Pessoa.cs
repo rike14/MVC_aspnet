b1aa5ff2861c3c9cd28145b4f28e0bd49f96920e
Henrique Kronhardt <henriquemk00@gmail.com>
2022-11-17 00:19:42 -0300

    Correção cadastrar pessoa

--- CRUD/Models/Pessoa.cs
+++ CRUD/Models/Pessoa.cs
@@ -12,31 +12,31 @@ namespace CRUD.Models
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [MaxLength(12, ErrorMessage = "O campo {0} não pode ser maior que {1} números")]
-        [StringLength(11, MinimumLength = 11, ErrorMessage = "O campo{0} tem que ter {1} números")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "O campo {0} tem que ter {1} números")]
         public long Cpf { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(150, MinimumLength = 15, ErrorMessage = "{0} tem que ter no mínimo {2} caracteres")]
         public string Endereco { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [MaxLength(4, ErrorMessage = "O campo {0} deve ser Ex: 011")]
-        [StringLength(3, MinimumLength = 3, ErrorMessage = "O campo {0} deve ser Ex: 011")]
+        [Required(ErrorMessage = "O campo DDD é obrigatório")]
+        [MaxLength(4, ErrorMessage = "O campo DDD deve ser Ex: 011")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "O campo DDD deve ser Ex: 011")]
         public int DDD_principal { get; set; }
 
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [MaxLength(10, ErrorMessage = "O campo {0} deve ser Ex: 988776655")]
-        [StringLength(9, MinimumLength = 8, ErrorMessage = "O campo {0} deve ser Ex: 988776655")]
+        [Required(ErrorMessage = "O campo Telefone Principal é obrigatório")]
+        [MaxLength(10, ErrorMessage = "O campo Telefone Principal deve ser Ex: 988776655")]
+        [StringLength(9, MinimumLength = 8, ErrorMessage = "O campo Telefone Principal deve ser Ex: 988776655")]
         public long Tel_principal { get; set; }
 
         
-        [MaxLength(4, ErrorMessage = "O campo {0} deve ser Ex: 011")]
-        [StringLength(3, MinimumLength = 3, ErrorMessage = "O campo {0} deve ser Ex: 011")]
+        [MaxLength(4, ErrorMessage = "O campo DDD deve ser Ex: 011")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "O campo DDD deve ser Ex: 011")]
         public int? DDD_secundario { get; set; }
 
         
-        [MaxLength(10, ErrorMessage = "O campo {0} deve ser Ex: 988776655")]
-        [StringLength(9, MinimumLength = 8, ErrorMessage = "O campo {0} deve ser Ex: 988776655")]
+        [MaxLength(10, ErrorMessage = "O campo Telefone Secundário deve ser Ex: 988776655")]
+        [StringLength(9, MinimumLength = 8, ErrorMessage = "O campo Telefone Secundário deve ser Ex: 988776655")]
         public long? Tel_secundario { get; set; }
     }
 }
